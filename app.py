69356120c03db15723a7ec011b24d7d0b8aeaee7
Daniel Corsano <daniel.corsano@gmail.com>
2021-02-26 15:10:28 +0100

    fixing frontend stuff

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 import requests
 
 '''
-# Predict your taxi fare!
+# Predict your New York taxi fare!
 '''
 
 key = "2012-10-06%2012:10:20.0000001"
@@ -45,6 +45,6 @@ if url == 'http://taxifare.lewagon.ai/predict_fare/':
     #response = requests.get(f"{url}predict_fare/?key={call_dict['key']}&pickup_datetime={call_dict['date']}%{time}&pickup_longitude={call_dict['pickup_lon']}&pickup_latitude={call_dict['pickup_lat']}&dropoff_longitude={call_dict['dropoff_lon']}&dropoff_latitude={call_dict['dropoff_lat']}&passenger_count={call_dict['passenger_count']}")
     if button == 1:
         prediction = requests.get(url, params=call_dict).json()['prediction']
-        st.markdown(f"## Your fare will cost {prediction}")
+        st.markdown(f"## Your fare will cost {prediction} $")
     else:
         st.markdown("## Press the button!")
